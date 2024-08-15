f1bde92ac8462aa780c8da8266c8a31f1a7d355d
Daniel Hash <hashdan542@gmail.com>
2024-08-15 22:06:14 +0200

    Onboarding page update

--- src/pages/ConnectPage/ConnectPage.tsx
+++ src/pages/ConnectPage/ConnectPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useTonAddress } from "@tonconnect/ui-react";
 import Logo from "../IndexPage/TELogo.svg";
 import { useTonConnectUI } from "@tonconnect/ui-react";
@@ -7,8 +7,12 @@ import { useNavigate } from "react-router-dom";
 import { useMutation } from "@tanstack/react-query";
 import { API } from "@/api/api";
 import { useInitData } from "@telegram-apps/sdk-react";
+import { useSwipeable } from "react-swipeable"; 
+import ScreenshotImage from '@/pages/ConnectPage/screenshots.png';
+import { IoMdWallet } from "react-icons/io";
 
 export const ConnectPage = () => {
+	const [currentSlide, setCurrentSlide] = useState(0);
 	const userFriendlyAddress = useTonAddress();
 	const navigate = useNavigate();
 	const initData = useInitData();
@@ -40,48 +44,75 @@ export const ConnectPage = () => {
 		tonConnectUI.openModal();
 	};
 
+	const handleNextSlide = () => {
+		postEvent("web_app_trigger_haptic_feedback", {
+			type: "impact",
+			impact_style: "soft",
+		});
+
+		if (currentSlide < 1) {
+			setCurrentSlide(currentSlide + 1);
+		} else {
+			connectHandleClick();
+		}
+	};
+
+	const handlePreviousSlide = () => {
+		if (currentSlide > 0) {
+			setCurrentSlide(currentSlide - 1);
+		}
+	};
+
+	const handlers = useSwipeable({
+		onSwipedLeft: () => handleNextSlide(),
+		onSwipedRight: () => handlePreviousSlide(),
+		preventScrollOnSwipe: true, 
+		trackMouse: true,
+	});
+
 	return (
-		<div className="h-screen flex flex-col bg-black p-4">
-			{/* Content container at the top */}
-			<div className="flex-grow flex flex-col justify-center items-center max-w-md w-full mx-auto mb-4">
-				<div className="text-center bg-zinc-900 rounded-xl p-4 text-gray-300 flex flex-col items-center">
-					<div className="logo-name mb-4 flex flex-col items-center">
-						<span className="w-20 h-20 rounded-full bg-black flex items-center justify-center mb-3">
-							<img className="text-yellow-300 h-8 p-1" src={Logo} alt="Logo" />
-						</span>
-						<h2 className="font-bold text-xl sm:text-2xl">Traction Eye</h2>
-					</div>
-
-					<h3 className="text-lg sm:text-xl mb-3">
-						Universal toolbar for investors
-					</h3>
-
-					<div className="text-base mt-2 mb-4">
-						<div className="mb-6">
-							<span className="text-4xl sm:text-5xl">💸</span>
-							<p className="mt-1 text-sm sm:text-base">
-								Analyse PnL and profitability of DeFi protocols on one screen
-							</p>
-						</div>
-
+		<div {...handlers} className="h-screen flex flex-col bg-black p-4 pt-2 select-none overflow-y-hidden">
+			{/* Content container */}
+			<div className="flex-grow flex flex-col justify-center items-center max-w-md w-full mx-auto">
+				<div className="text-center rounded-xl p-4 py-0 text-gray-300 flex flex-col items-center">
+					{currentSlide === 0 ? (
 						<div>
-							<span className="text-4xl sm:text-5xl">📊</span>
-							<p className="mt-1 text-sm sm:text-base">
-								Track the onchain activity of influencers you trust and earn
-								with them
-							</p>
+							<span className="rounded-full bg-black flex items-center justify-center mb-2">
+								<img className="w-32 h-32" src={Logo} alt="Logo" />
+							</span>
+							<h2 className="font-bold text-2xl sm:text-3xl text-transparent bg-clip-text bg-gradient-to-r from-yellow-200 via-yellow-500 to-yellow-600 mb-6">
+								Welcome to real web3 vision of investing
+							</h2>
 						</div>
-					</div>
+					) : (
+						<>
+							<h3 className="text-lg font-semibold sm:text-xl lg:text-2xl mb-2">
+								Analyse Profit and Loss <br/> of any kind of assets
+							</h3>
+							<div className="relative w-full max-w-xs h-auto mx-auto mb-0 mb-3">
+								<img className="w-full h-62 " src={ScreenshotImage} alt="" />
+							</div>
+							<h3 className="text-lg font-semibold sm:text-xl lg:text-2xl mb-3">
+								All DeFi protocols on one screen
+							</h3>
+						</>
+					)}
 				</div>
 			</div>
 
-			{/* Button container at the bottom */}
-			<div className="flex-none sticky bottom-0 flex justify-center mt-auto pb-4 w-full">
+			{/* Slide indicators */}
+			<div className="flex justify-center mb-3">
+				<span className={`w-2 h-2 mx-1 rounded-full ${currentSlide === 0 ? 'bg-yellow-300' : 'bg-gray-600'}`}></span>
+				<span className={`w-2 h-2 mx-1 rounded-full ${currentSlide === 1 ? 'bg-yellow-300' : 'bg-gray-600'}`}></span>
+			</div>
+
+			{/* Button container */}
+			<div className="flex-none flex justify-center pb-10 w-full">
 				<button
-					onClick={connectHandleClick}
-					className="bg-yellow-400 py-4 px-8 rounded-2xl w-full sm:w-3/4 md:w-1/2 lg:w-40 flex justify-center items-center text-base sm:text-lg"
+					onClick={handleNextSlide}
+					className="bg-yellow-400 py-3 px-6 rounded-2xl w-full sm:w-3/4 md:w-1/2 lg:w-1/3 flex justify-center items-center text-base sm:text-lg"
 				>
-					Connect Wallet
+					{currentSlide === 0 ? "Continue" : <span className="flex items-center">Connect Wallet<IoMdWallet className="text-lg ml-2"/></span>}
 				</button>
 			</div>
 		</div>
