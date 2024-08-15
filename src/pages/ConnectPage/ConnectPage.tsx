e449555d69fc26f170ec837f808ad19d6b5e246b
barrydillan <queharambe@gmail.com>
2024-08-15 18:11:29 +0200

    fix: fix redirect

--- src/pages/ConnectPage/ConnectPage.tsx
+++ src/pages/ConnectPage/ConnectPage.tsx
@@ -1,8 +1,8 @@
 import { useEffect } from "react";
 import { useTonAddress } from "@tonconnect/ui-react";
 import Logo from "../IndexPage/TELogo.svg";
-import { useTonConnectUI } from '@tonconnect/ui-react';
-import { postEvent } from '@telegram-apps/sdk';
+import { useTonConnectUI } from "@tonconnect/ui-react";
+import { postEvent } from "@telegram-apps/sdk";
 import { useNavigate } from "react-router-dom";
 import { useMutation } from "@tanstack/react-query";
 import { API } from "@/api/api";
@@ -11,27 +11,32 @@ import { useInitData } from "@telegram-apps/sdk-react";
 export const ConnectPage = () => {
 	const userFriendlyAddress = useTonAddress();
 	const navigate = useNavigate();
-  const initData = useInitData();
+	const initData = useInitData();
 
-  const mutation = useMutation(
-    {
-      mutationFn: (params: {telegram_id: number, wallet_address: string}) => API.addWallet(params.telegram_id, params.wallet_address),
-      mutationKey: ['add-wallet'],
-      onSuccess: () => {
-		navigate("/");
-      }
-    }
-  );
+	const mutation = useMutation({
+		mutationFn: (params: { telegram_id: number; wallet_address: string }) =>
+			API.addWallet(params.telegram_id, params.wallet_address),
+		mutationKey: ["add-wallet"],
+		onSettled: () => {
+			navigate("/");
+		},
+	});
 
 	useEffect(() => {
 		if (!userFriendlyAddress) return;
-		mutation.mutate({telegram_id: initData?.user?.id ? initData?.user?.id : 0, wallet_address: userFriendlyAddress })
+		mutation.mutate({
+			telegram_id: initData?.user?.id ? initData?.user?.id : 0,
+			wallet_address: userFriendlyAddress,
+		});
 	}, [userFriendlyAddress]);
 
 	const [tonConnectUI] = useTonConnectUI();
 
 	const connectHandleClick = () => {
-		postEvent('web_app_trigger_haptic_feedback', { type: 'notification', notification_type: 'warning' });
+		postEvent("web_app_trigger_haptic_feedback", {
+			type: "notification",
+			notification_type: "warning",
+		});
 		tonConnectUI.openModal();
 	};
 
@@ -47,7 +52,9 @@ export const ConnectPage = () => {
 						<h2 className="font-bold text-xl sm:text-2xl">Traction Eye</h2>
 					</div>
 
-					<h3 className="text-lg sm:text-xl mb-3">Universal toolbar for investors</h3>
+					<h3 className="text-lg sm:text-xl mb-3">
+						Universal toolbar for investors
+					</h3>
 
 					<div className="text-base mt-2 mb-4">
 						<div className="mb-6">
@@ -60,8 +67,8 @@ export const ConnectPage = () => {
 						<div>
 							<span className="text-4xl sm:text-5xl">📊</span>
 							<p className="mt-1 text-sm sm:text-base">
-								Track the onchain activity of influencers you trust and earn with
-								them
+								Track the onchain activity of influencers you trust and earn
+								with them
 							</p>
 						</div>
 					</div>
@@ -70,8 +77,8 @@ export const ConnectPage = () => {
 
 			{/* Button container at the bottom */}
 			<div className="flex-none sticky bottom-0 flex justify-center mt-auto pb-4 w-full">
-				<button 
-					onClick={connectHandleClick} 
+				<button
+					onClick={connectHandleClick}
 					className="bg-yellow-400 py-4 px-8 rounded-2xl w-full sm:w-3/4 md:w-1/2 lg:w-40 flex justify-center items-center text-base sm:text-lg"
 				>
 					Connect Wallet
