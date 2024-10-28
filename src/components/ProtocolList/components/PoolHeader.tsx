062010dd27f753681fef4f662ecac51d596d2662
Pavel Meyer <meyerpavel1207@gmail.com>
2024-10-28 21:32:40 +0300

    Update PoolHeader.tsx

--- src/components/ProtocolList/components/PoolHeader.tsx
+++ src/components/ProtocolList/components/PoolHeader.tsx
@@ -7,7 +7,6 @@ interface PoolHeaderProps {
 
 export const PoolHeader: FC<PoolHeaderProps> = ({ icon, poolName }) => {
 
-  console.log('--icon',icon);
   return (
     <div className="flex items-center min-h-8">
       {(typeof icon === "string") ? <img src={icon} alt={poolName} className="rounded-lg h-8 w-8 mr-2" /> :  icon}
