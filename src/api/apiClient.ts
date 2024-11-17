23a4aceab6d7b598da982a2c66f2d2e4e7e9b6f0
Pavel Meyer <meyerpavel1207@gmail.com>
2024-11-17 11:14:26 +0300

    Update apiClient.ts

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -2,7 +2,7 @@ import { LocalStorageKeys } from '@/constants/localStorage';
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://tarctioneye.ru/backend_api/',
+  baseURL: 'https://tractioneye.ru/backend_api/',
   headers: {
     'Content-Type': 'application/json',
   },
