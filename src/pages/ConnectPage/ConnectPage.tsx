cb089fdebe3f7f4b30f8e7428e67912497a64ab9
barrydillan <queharambe@gmail.com>
2024-11-22 15:49:15 +0100

    build

--- src/pages/ConnectPage/ConnectPage.tsx
+++ src/pages/ConnectPage/ConnectPage.tsx
@@ -14,6 +14,7 @@ import { IoMdWallet } from "@/components/icons";
 import "./ConnectPage.css"; // Импортируйте CSS файл
 import { Spinner } from "@/components/ui/spinner";
 import { LocalStorageKeys } from "@/constants/localStorage";
+//fuck shit
 
 export const ConnectPage = () => {
 	const [currentSlide, setCurrentSlide] = useState(0);
