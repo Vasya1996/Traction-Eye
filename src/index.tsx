4472451cd32bca024bb65ca97c704cdf17724474
Pavel Meyer <meyerpavel1207@gmail.com>
2024-12-06 18:55:43 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,7 @@ import ReactDOM from 'react-dom/client';
 import { Root } from '@/components/Root';
 import ReactGA from "react-ga4";
 
-ReactGA.initialize("G-F9LNTCTMS1");
-// ReactGA.initialize("G-F7VJLGRX7L");
+ReactGA.initialize("G-F7VJLGRX7L");
 
 // Uncomment this import in case, you would like to develop the application even outside
 // the Telegram application, just in your browser.
